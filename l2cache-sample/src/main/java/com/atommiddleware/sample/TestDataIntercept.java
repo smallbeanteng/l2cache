e0fa011091d9c9c44b38a7ee70d98521242f30c0
smallbeanteng <smallbeanteng@163.com>
2021-12-19 08:45:11 +0800

    modify pom

--- l2cache-sample/src/main/java/com/atommiddleware/sample/TestDataIntercept.java
+++ l2cache-sample/src/main/java/com/atommiddleware/sample/TestDataIntercept.java
@@ -13,26 +13,28 @@ import com.atommiddleware.cache.intercept.DataMember;
 
 @Component
 public class TestDataIntercept {
-
 	@Autowired
 	private DataInterceptor dataInterceptor;
-	
+
 	@PostConstruct
 	public void init() {
-		dataInterceptor.add(new DataMember("user","1246"),DataType.WHITE);
-		dataInterceptor.add(new DataMember("user","158"),DataType.BLACK);
+		dataInterceptor.add(new DataMember("user", "1246"), DataType.WHITE);
+		dataInterceptor.add(new DataMember("user", "158"), DataType.BLACK);
 	}
-	@DataIntercept(prefix = "user",key="#id")
+
+	@DataIntercept(prefix = "user", key = "#id")
 	public void validate(String id) {
-	System.out.println("穿过了"+id);	
+		System.out.println("穿过了" + id);
 	}
+
 	/**
 	 * 可以跟随@Cacheable的配置 cacheNames[0]=prefix key=key
+	 * 
 	 * @param id
 	 */
 	@DataIntercept
-	@Cacheable(cacheNames = "user",key="#id")
+	@Cacheable(cacheNames = "user", key = "#id")
 	public void validateWithCache(String id) {
-		System.out.println("穿过了"+id);	
+		System.out.println("穿过了" + id);
 	}
 }
